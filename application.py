db3c695f849f3d6a752bee33ab991344176309cb
krishna <krishna@localhost.localdomain>
2019-02-16 11:31:04 +0530

    Online compiler

--- application.py
+++ application.py
@@ -1,8 +1,28 @@
-from flask import Flask
+from flask import Flask,request,render_template
+import os
 app = Flask(__name__)
-
+st=''
 @app.route("/")
+def my_form():
+	return render_template('load.html')
+@app.route('/h',methods=['POST'])
 def hello():
-    return "Hello World!"
-#if __name__="__main__":
-#    app.run()
+	#text=request.args.get('python_inp')
+	global st
+	text=request.form['javascript_data']
+	values=request.form['cust_in']
+	text="import sys\n"+text
+	arg=values.split(",")
+	cmd1=[]
+	cmd='python3 /home/krishna/Desktop/firs/scrip.py'
+	cmd1.append(cmd)
+	cmd1=cmd1+arg
+	cmd1=" ".join(cmd1)
+	f=open('scrip.py','w')
+	f.write(text)
+	f.close()
+	a=os.popen(cmd1).readlines()
+	st=''.join(a)
+	return st
+app.run(debug=True)
+
